45c3afc0106b2891e5b6ea89bb4563e05b746873
tanjung mega abdurrais <tanjung.mega.1904136@students.um.ac.id>
2025-06-17 07:53:08 +0700

    Update auto_order.py

--- auto_order.py
+++ auto_order.py
@@ -6,7 +6,7 @@ API_URL = "https://www.jagososmed.com/api/json.php"
 
 SERVICE_ID = 5735
 TARGET_DATA = "https://www.tiktok.com/@anishclip/video/7513911683279113490"
-QUANTITY = 55
+QUANTITY = 10
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
