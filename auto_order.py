efb7133dd0941e70babddf9a4cde3b4d8866a82d
tanjung mega abdurrais <tanjung.mega.1904136@students.um.ac.id>
2025-06-21 05:42:03 +0700

    Update auto_order.py

--- auto_order.py
+++ auto_order.py
@@ -4,9 +4,9 @@ import requests
 API_KEY = "CNDM6NOEqtBJxL4vaUo7UGs8"
 API_URL = "https://www.jagososmed.com/api/json.php"
 
-SERVICE_ID = 5735
-TARGET_DATA = "https://www.tiktok.com/@anishclip/video/7513930717198404871"
-QUANTITY = 33
+SERVICE_ID = 5587
+TARGET_DATA = "https://www.tiktok.com/@itsshieubank/video/7518154461710339346"
+QUANTITY = 11
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
